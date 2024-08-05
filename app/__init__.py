1056d2c7902188229d40ba9ffd064971c472ed13
Heraa Farooq <93732214+techcity1985@users.noreply.github.com>
2024-08-05 23:11:22 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from .routes.auth import auth
 from .routes.main import main
+from .models import db
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
+
+    db.init_app(app)
 
     app.register_blueprint(auth)
     app.register_blueprint(main)
