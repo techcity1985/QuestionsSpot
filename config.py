62b0fa859ab1df7aff3ec91d9af77439dcb7a2dc
Heraa Farooq <93732214+techcity1985@users.noreply.github.com>
2024-08-05 23:08:11 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 class Config:
-    SECRET_KEY = 'your_very_secret_key_here'
-    # Add database configurations here if needed
+    SECRET_KEY = 'your_very_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///questionspot.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
