79c72ec48cec1f4aaca74c83fd15533ede25a967
Heraa Farooq <93732214+techcity1985@users.noreply.github.com>
2024-08-05 23:14:00 +0300

    Update question.py

--- app/models/question.py
+++ app/models/question.py
@@ -1,9 +1,6 @@
-from .user import db
+from . import db
 
 class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     question_text = db.Column(db.Text, nullable=False)
     answer = db.Column(db.Text, nullable=False)
-
-    def __repr__(self):
-        return '<Question %r>' % self.question_text
